338998516d9fb9a4d0bf97c76947fa223bd448b5
iqbalfarhan <iqbalfarhan1996@gmail.com>
2025-08-26 12:39:13 +0800

    Enhances user permissions handling in controller stubs and updates filter sheet stubs for better access control

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+
 abstract class Controller
 {
-    //
+    protected ?User $user;
+
+    public function __construct()
+    {
+        $this->user = auth()->user();
+    }
 }
